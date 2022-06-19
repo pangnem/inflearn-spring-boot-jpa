39e69b4b959a37a32ad3da23eba0ac8d95ef1467
pangnem <sprnd645@gmail.com>
2022-06-19 16:10:28 +0900

    엔티티 클래스 개발

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "member_id")
+	private Long id;
+
+	private String name;
+
+	@Embedded
+	private Address address;
+
+	@OneToMany(mappedBy = "member") // order 테이블의 member 필드에 의해서 매핑됨을 알림(읽기 전용)
+	private List<Order> orders = new ArrayList<>();
+}
