f63004f3e8a415b808519fb3ab7b8561732856dc
pangnem <sprnd645@gmail.com>
2022-06-22 01:03:29 +0900

    주문 레포지토리 개발

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+	private final EntityManager entityManager;
+
+	public void save(Order order) {
+		entityManager.persist(order);
+	}
+
+	public Order findOne(Long id) {
+		return entityManager.find(Order.class, id);
+	}
+
+//	public List<Order> findAll(OrderSearchDto orderSearchDto) {}
+}
