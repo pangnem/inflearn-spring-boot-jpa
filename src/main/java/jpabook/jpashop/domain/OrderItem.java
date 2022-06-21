446abbd9735bf2357c075dcc06318acfc9d1383b
pangnem <sprnd645@gmail.com>
2022-06-22 01:00:00 +0900

    주문 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -32,4 +32,24 @@ public class OrderItem {
 
 	private int orderPrice; // 주문 가격
 	private int count; // 주문 수량
+
+	//==생성 로직==//
+	public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+		OrderItem orderItem = new OrderItem();
+		orderItem.setItem(item);
+		orderItem.setOrderPrice(orderPrice);
+		orderItem.setCount(count);
+
+		item.removeStock(count);
+		return orderItem;
+	}
+
+	//==비즈니스 로직==//
+	public void cancel() {
+		item.addStock(count); // 재고수량 원복
+	}
+
+	public int getTotalPrice() {
+		return orderPrice * count;
+	}
 }
