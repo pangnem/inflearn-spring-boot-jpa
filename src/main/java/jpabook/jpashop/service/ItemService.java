059712d4ca7792331687cfd30f820139b2df18b1
pangnem <sprnd645@gmail.com>
2022-06-25 19:08:14 +0900

    상품 서비스 수정 메서드 하나 더 추가

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,17 @@ public class ItemService {
 	@Transactional
 	public void saveItem(Item item) {
 		itemRepository.save(item);
+	}
+
+	@Transactional
+	public void updateItem(Long itemId, Book book) {
+		Item findItem = itemRepository.findOne(itemId);
+		findItem.setPrice(book.getPrice());
+		findItem.setName(book.getName());
+		findItem.setStockQuantity(book.getStockQuantity());
+
+		// 변경 감지가 일어나서 호출하지 않아도 된다!
+//		itemRepository.save(findItem);
 	}
 
 	public List<Item> findItems() {
