50cb6e33376add0e237c5abcf3aebce8eb9c5580
pangnem <sprnd645@gmail.com>
2022-07-02 19:22:19 +0900

    주문 조회 entity v3.1 - batch_size 옵션 활성화
    일대다 관계에서는 지연 로딩을 계속 유지하되, batch_size를 활성화해서
    한번에 가져올 것들을 지정한다.
    - 단, xxxToOne 관계에서는 fetch join을 해도 데이터 개수가 증가하지 않기
      때문에 페이징에 대한 이슈는 없다.

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -9,6 +9,7 @@ import jpabook.jpashop.repository.OrderSearch;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -55,6 +56,17 @@ public class OrderApiController {
 				.collect(Collectors.toList());
 	}
 
+	@GetMapping("/api/v3.1/orders")
+	public List<OrderDto> ordersV3OrderList_page(
+			@RequestParam(value = "offset", defaultValue = "0") int offset,
+			@RequestParam(value = "limit", defaultValue = "100") int limit
+			) {
+		List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+
+		return orders.stream()
+				.map(OrderDto::new)
+				.collect(Collectors.toList());
+	}
 
 
 	@Data
