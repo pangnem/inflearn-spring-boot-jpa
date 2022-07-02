9f214722ca242b5d493adae11ebb159d1a5acfdd
pangnem <sprnd645@gmail.com>
2022-07-02 16:22:17 +0900

    엔티티를 DTO로 변환
    N+1 문제가 발생하여 fetch join으로 최적화 해야함

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne 성능 최적화
@@ -33,5 +38,31 @@ public class OrderSimpleApiController {
 		}
 
 		return orders;
+	}
+
+	@GetMapping("/api/v2/simple-orders")
+	public List<SimpleOrderDto> ordersV2() {
+		List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+
+		return orders.stream()
+				.map(SimpleOrderDto::new)
+				.collect(Collectors.toList());
+	}
+
+	@Data
+	static class SimpleOrderDto {
+		private Long orderId;
+		private String name;
+		private LocalDateTime orderDate;
+		private OrderStatus orderStatus;
+		private Address address;
+
+		public SimpleOrderDto(Order order) {
+			this.orderId = order.getId();
+			this.name = order.getMember().getName(); // LAZY 초기화
+			this.orderDate = order.getOrderDate();
+			this.orderStatus = order.getStatus();
+			this.address = order.getMember().getAddress(); // LAZY 초기화
+		}
 	}
 }
